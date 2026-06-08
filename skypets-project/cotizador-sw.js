f30d120b3c465a251d10ee57a7a9ae5a9c3fdf8b
andysanchezcol <andysanchezcol@gmail.com>
2026-06-08 13:41:48 -0500

    fix: bump SW cache v3 para forzar actualización

--- skypets-project/cotizador-sw.js
+++ skypets-project/cotizador-sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'skypets-cotizador-v1';
+const CACHE = 'skypets-cotizador-v3';
 const ASSETS = [
   '/skypetscol/skypets-project/cotizador.html',
   '/skypetscol/skypets-project/assets/images/Logo_SkyPets.webp'
